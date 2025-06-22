cd62701da6fb52c7e3719658454f96a375011f76
Adithya kiran C B <adithyakiran021@gmail.com>
2025-06-22 23:28:36 +0530

    till 1024px finished

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
 const menuBtn = document.querySelector(".menu-button")
 const dropDown =document.querySelectorAll(".category-item-link")
 const subMenus = document.querySelectorAll(".category-sub-items")
+const follow = document.querySelector(".form-button")
+const followDropDown = document.getElementById("follow-button")
+
 menuBtn.addEventListener("click", () => {
     document.body.toggleAttribute("data-category-mobile-menu-open")
 })
 
 dropDown.forEach((item, index) => {
-    item.addEventListener("click", (e) =>{
-        e.preventDefault();
+    item.addEventListener("click", (e) => {
+        e.preventDefault()
 
         const subMenu = subMenus[index]
-        const isVisible = subMenu.style.height === "auto"
+        const isOpen = subMenu.classList.contains("open")
 
-        subMenus.forEach(menu => {menu.style.display = "0"})
-        
-        subMenu.style.height = isVisible ? "0" : "auto"
+        subMenus.forEach((menu) => {
+            menu.style.height = "0"
+            menu.classList.remove("open")
+        })
+
+        if(!isOpen){
+            const fullHeight = subMenu.scrollHeight + "px"
+            subMenu.style.height = fullHeight
+            subMenu.classList.add("open")
+        }
     })
-}) 
+})
+
+follow.addEventListener("click", () =>{
+    followDropDown.classList.toggle("visible")
+})
